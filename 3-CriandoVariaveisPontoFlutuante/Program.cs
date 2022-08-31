600a2ac31be6416991aa9e9bd5e358dd89cfe064
Andriele Joras dos Santos <andrielejorasdossantos@gmail.com>
2022-08-31 14:29:07 -0300

    :construction: Projeto 3 e 4

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+        double salario = 3000.10;
+
+        Console.WriteLine(salario);
+        Console.WriteLine("meu salario é " + salario);
+    }
+}
---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        //tipo mais comum em relação a números com , 
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        //short armazena até 16 bits (2^16) = 16000
+        short y = 15000;
+        Console.WriteLine(y);
+
+        //int armazena até 32 bits (2^32) = 200.000.000.000
+        int salarioInteiro = (int)salario; //parseando
+        Console.WriteLine(salarioInteiro);
+
+        //long armazena 64 bits (2^64) = 2.000.000.000.000.000.000
+        long x = 200000000000000000;
+        Console.WriteLine(x);
+
+        //outro tipo de double
+        float altura = 1.62f; //sufixo f para avisar que queremos utilizar float
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
