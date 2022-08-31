26a22a3cff7ebde7aae7ad1286f53647b05207c4
Andriele Joras dos Santos <andrielejorasdossantos@gmail.com>
2022-08-31 10:47:15 -0300

    :construction: Projeto 2

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+        
+        int idade; //Declarar o tipo
+        idade = 27;
+
+        Console.WriteLine(idade);
+
+        idade = (5 * 2) - 6;
+
+        Console.WriteLine(idade);
+
+        //Console.Write(); não pula linha
+
+        Console.WriteLine("Tecle enter para fechar.");
+        Console.ReadLine();
+
+    }
+}
